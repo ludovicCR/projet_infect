0edea859506f60c8c5ad493a966250b444570818
ludovic <ludovic.crousse@gmail.com>
2023-12-06 12:31:58 +0100

    Initialisation et commencement

---
+++ projet_infectee.py
@@ -0,0 +1,20 @@
+#initiation variable
+population = 11500000
+nombre_infectes = 100
+rencontre_jour = 10
+PT = 0.01
+PG = 0.05
+
+#liste
+liste_infectes = [nombre_infectes]
+liste_jours = [1]
+
+#Boucle pour calculer nombre infecté chaque jours !
+for jour in range(2,366):
+    if 30 <= jour <= 75:
+        rencontre_jour = 3
+    niag = liste_infectes[jour-2] * (1 - PG)
+    nombre_infectes = niag + (population - niag) * rencontre_jour * niag / population * PT
+    liste_infectes.append(nombre_infectes)
+    liste_jours.append(jour)
+
