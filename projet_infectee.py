a5910b9e0527c73d50946633048dc9967aab1e37
ludovic <ludovic.crousse@gmail.com>
2023-12-06 15:05:26 +0100

    Réparation et nouveau test

--- projet_infectee.py
+++ projet_infectee.py
@@ -1,4 +1,4 @@
-#initiation variable
+
 population = 11500000
 nombre_infectes = 100
 rencontre_jour = 10
@@ -8,15 +8,24 @@ PG = 0.05
 #liste
 liste_infectes = [nombre_infectes]
 liste_jours = [1]
+liste_nouvelles_infections = [nombre_infectes]
 
 #Boucle pour calculer nombre infecté chaque jours !
-for jour in range(2,366):
+for jour in range(0,365):
     if 30 <= jour <= 75:
         rencontre_jour = 3
-    niag = liste_infectes[jour-2] * (1 - PG)
+    else:
+        rencontre_jour = 10
+    niag = liste_infectes[jour] * (1 - PG)
     nombre_infectes = niag + (population - niag) * rencontre_jour * niag / population * PT
+
     liste_infectes.append(nombre_infectes)
     liste_jours.append(jour)
+
+    liste_nouvelles_infections.append(nombre_infectes - liste_infectes[jour])
+
+#Calcul nombre moyen de personne infecté par jour.
+nombre_moyen_infectes = sum(liste_infectes) / len(liste_infectes)
 
 #Affichage des résultats
 while True:
@@ -27,3 +36,4 @@ while True:
         print("Jour invalide, veuillez entrez un nombre entre 1 et 365")
     else: 
         print("Jour",jour,":",round(liste_infectes[jour-1],2), "personnes infectées")
+print("Le nombre moyen de personnes infectés par jour sur l'ensemble de l'année est de :",round(nombre_moyen_infectes,2))
